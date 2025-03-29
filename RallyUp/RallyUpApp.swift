9e90d3f7d052ec398d105f89a91f936602ecd02b
Rishikumar Senthilvel <rsenthi4@asu.edu>
2025-03-29 15:15:11 -0700

    Initial Commit

---
+++ RallyUp/RallyUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  RallyUpApp.swift
+//  RallyUp
+//
+//  Created by RISHIKUMAR on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct RallyUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RallyUpTests/RallyUpTests.swift
@@ -0,0 +1,17 @@
+//
+//  RallyUpTests.swift
+//  RallyUpTests
+//
+//  Created by RISHIKUMAR on 3/29/25.
+//
+
+import Testing
+@testable import RallyUp
+
+struct RallyUpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
